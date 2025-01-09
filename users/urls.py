4757d43a554cc6383ddd7fc0bf86f674e600a742
DanStaf <danstaf@mail.ru>
2025-01-09 12:36:01 +0300

    Users CRUD, login, register, email-verification added

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from rest_framework import routers
 from rest_framework.permissions import AllowAny
 
-#from users.views import UserViewSet
+from users.views import UserViewSet, email_verification
 from users.apps import UsersConfig
 from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView,
@@ -9,6 +9,24 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
 
 app_name = UsersConfig.name
 
+router_user = routers.DefaultRouter()
+router_user.register("", UserViewSet, basename='user')
+
 urlpatterns = [
 
+    path('login/',
+         TokenObtainPairView.as_view(
+             permission_classes=(AllowAny,)
+         ),
+         name='login'),
+    path('token/refresh/',
+         TokenRefreshView.as_view(
+             permission_classes=(AllowAny,)
+         ),
+         name='token_refresh'),
+
+    path('email_confirm/<str:token>/', email_verification, name='email_confirm'),
+
 ]
+
+urlpatterns += router_user.urls
--- users/views.py
+++ users/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+import secrets
+from django.core.mail import send_mail
+from config.settings import EMAIL_HOST_USER, LOGIN_REDIRECT_URL
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        new_user = serializer.save(is_active=True)
+        new_user.set_password(new_user.password)
+
+        new_user.is_active = False
+        token = secrets.token_hex(16)
+        new_user.token = token
+
+        new_user.save()
+
+        host = self.request.get_host()
+        url = f'http://{host}/users/email_confirm/{token}/'
+
+        send_mail(
+            "DF 1 Library - email confirm",
+            f"To confirm the email please tap on the link: {url}",
+            EMAIL_HOST_USER,
+            [new_user.email],
+            fail_silently=False,
+        )
+
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = [AllowAny]
+        else:
+            self.permission_classes = [IsAuthenticated]
+
+        return super().get_permissions()
+
+
+def email_verification(request, token):
+    user = get_object_or_404(User, token=token)
+    user.is_active = True
+    user.save()
+
+    return redirect(reverse("users:user-list"))
