69b188814578d8df3c55b13c2cf0166993fbd94d
DanStaf <danstaf@mail.ru>
2025-01-10 00:57:48 +0300

    docker fixed

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -5,40 +5,43 @@ from users.models import User
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        if not User.objects.filter(email='danstaf@mail.ru'):
 
-        user = User.objects.create(
-            email='danstaf@mail.ru',
-            first_name='Admin',
-            last_name='Admin',
-            is_staff=True,
-            is_superuser=True,
-            is_manager=True
-        )
+            user = User.objects.create(
+                email='danstaf@mail.ru',
+                first_name='Admin',
+                last_name='Admin',
+                is_staff=True,
+                is_superuser=True,
+                is_manager=True
+            )
 
-        user.set_password('admin')
-        user.save()
+            user.set_password('admin')
+            user.save()
 
-        user = User.objects.create(
-            email='danstaf1@mail.ru',
-            first_name='Reader',
-            last_name='Reader',
-            is_staff=False,
-            is_superuser=False
-        )
+        if not User.objects.filter(email='danstaf1@mail.ru'):
+            user = User.objects.create(
+                email='danstaf1@mail.ru',
+                first_name='Reader',
+                last_name='Reader',
+                is_staff=False,
+                is_superuser=False
+            )
 
-        user.set_password('12345')
-        user.save()
+            user.set_password('12345')
+            user.save()
 
-        user = User.objects.create(
-            email='danstaf2@mail.ru',
-            first_name='Manager',
-            last_name='Manager',
-            is_staff=False,
-            is_superuser=False,
-            is_manager=True
-        )
+        if not User.objects.filter(email='danstaf2@mail.ru'):
+            user = User.objects.create(
+                email='danstaf2@mail.ru',
+                first_name='Manager',
+                last_name='Manager',
+                is_staff=False,
+                is_superuser=False,
+                is_manager=True
+            )
 
-        user.set_password('12345')
-        user.save()
+            user.set_password('12345')
+            user.save()
 
-        print("Users created")
+        print("Users created (if needed)")
